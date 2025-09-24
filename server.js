27b196acba46845a70c07ae11d7ddc2d4a5b741b
akos060316 <akos060316@gmail.com>
2025-09-24 10:30:47 +0200

    Post es Delete vegpontok elkeszitese

--- server.js
+++ server.js
@@ -17,22 +17,31 @@ app.get('/hello', (req, res) => {
     res.send("Hello itt az Express webszerver!");
 })
 
-app.get('/api/courses', (req, res) => {
+app.get('/api.courses', (req, res) => {
     res.json(courses);
 })
 
-app.get('/api/courses/:id', (req, res) => {
+app.get('/api.courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
     if (!course) res.status(404).send('A megadott azonosítóval nem található kurzus!');
     res.json(course);
 })
 
-app.post('/api/courses', (req, res) => {
+app.post('/api.courses', (req, res) => {
     const course = {
         id: courses.length + 1,
         name: req.body.name};
     courses.push(course);
-    res.json(req.body);
+    res.status(200).json({message: "Új elem hozzáadva!", data: req.body});
+})
+
+app.delete('/api.courses/:id' , (res,req) => {
+    const course = courses.find(c =>c.id === parseInt(req.params.id));
+     if (!course) res.status(404).send('A megadott id_vel nem létezik kurzus!');
+     const index = courses.indexOf(course);
+     courses.splice(index, 1);
+     res.status(200).json({message: "Kurzus törölve!", data: course});
+     
 })
 
 //A webszerver elindítása
